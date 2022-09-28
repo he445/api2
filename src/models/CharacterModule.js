b45a8af03cd8df60be164c384a9d58ea8520dabd
he445 <103337053+he445@users.noreply.github.com>
2022-09-28 15:27:56 -0300

    modify type of text

--- src/models/CharacterModule.js
+++ src/models/CharacterModule.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const characterSchema = new mongoose.Schema({
   user: { type: String },
   title: { type: String, required: true },
-  text: { type: String, required: true },
+  text: { type: Array, required: true },
 });
 
 module.exports = Character = mongoose.model('character', characterSchema);
