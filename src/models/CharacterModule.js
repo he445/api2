58b8ff69d3fe0e7ea9b26984571ee58d6ce9393e
he445 <103337053+he445@users.noreply.github.com>
2022-10-11 18:37:29 -0300

    feet remodeling module

--- src/models/CharacterModule.js
+++ src/models/CharacterModule.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
   user: { type: String },
-  title: { type: String, required: true },
-  text: { type: Array, required: true },
+  text: { type: String, required: true },
 });
 
 module.exports = Character = mongoose.model('character', characterSchema);
