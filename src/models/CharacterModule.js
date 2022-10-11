9e2efcffc102598025afff8f03a65cc696de9f44
he445 <103337053+he445@users.noreply.github.com>
2022-10-11 18:53:08 -0300

    01

--- src/models/CharacterModule.js
+++ src/models/CharacterModule.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
   user: { type: String },
+  title: { type: String, required: true },
   text: { type: String, required: true },
 });
 
